cedb8aecce81822e937bdb450fcb403e3d1fc2d8
wehmam <readytosurff@gmail.com>
2020-03-06 00:11:01 +0700

    konten 4

--- web.js
+++ web.js
@@ -7,13 +7,13 @@ var first_aCSS = document.querySelector('.gambar')
 first_aCSS.style.backgroundImage = "url(program1.jpg)"
 first_aCSS.style.minHeight = "500px"
 first_aCSS.style.backgroundAttachment = "fixed"
-first_aCSS.style.backgorundRepeat ="no-repeat"
+first_aCSS.style.backgorundRepeat = "no-repeat"
 first_aCSS.style.backgroundSize = "cover"
 first_aCSS.style.color = "white"
 first_aCSS.style.opacity = "0.8"
 
 var second_a = document.querySelectorAll('div')[1]
-second_a.className ="row"
+second_a.className = "row"
 
 var third_a = document.querySelectorAll('div')[2]
 third_a.className = "col-md-12"
@@ -28,20 +28,20 @@ var second_h1 = document.querySelectorAll('h1')[1]
 second_h1.className = "display-3"
 
 // css button
-    var first_button = document.querySelectorAll('button')[0]
-    first_button.className = "daftar"
-    first_button.style.backgroundColor = "blue"
-    first_button.style.border = "none"
-    first_button.style.color = "white"
-    first_button.style.padding = "20px"
-    first_button.style.textAlign = "center"
-    first_button.style.textDecoration = "none"
-    first_button.style.display = "inline-block"
-    first_button.style.fontSize = "16px"
-    first_button.style.margin = "4px 2px"
-    first_button.style.borderRadius = "40px"
+var first_button = document.querySelectorAll('button')[0]
+first_button.className = "daftar"
+first_button.style.backgroundColor = "blue"
+first_button.style.border = "none"
+first_button.style.color = "white"
+first_button.style.padding = "20px"
+first_button.style.textAlign = "center"
+first_button.style.textDecoration = "none"
+first_button.style.display = "inline-block"
+first_button.style.fontSize = "16px"
+first_button.style.margin = "4px 2px"
+first_button.style.borderRadius = "40px"
 
-    // css icon play
+// css icon play
 var first_i = document.querySelectorAll('i')[0]
 first_i.className = "fa fa-play-circle"
 first_i.style.fontSize = "72px"
@@ -69,13 +69,13 @@ divThird2.className = "row"
 var divThird3 = document.querySelectorAll('div')[10]
 divThird3.className = "col-md-6 mt-5 mb-5"
 
-var divThird4 =document.querySelectorAll('div')[11]
-divThird4.className = "col-md-6 mt-5 mb-5" 
+var divThird4 = document.querySelectorAll('div')[11]
+divThird4.className = "col-md-6 mt-5 mb-5"
 
 var divThird6 = document.querySelectorAll('div')[12]
 divThird6.className = "row"
 
-var divThird7 =document.querySelectorAll('div')[13]
+var divThird7 = document.querySelectorAll('div')[13]
 divThird7.className = "col-md-6 mt-5 mb-5"
 
 var divThird8 = document.querySelectorAll('div')[14]
@@ -83,6 +83,7 @@ divThird8.className = "col-md-6"
 
 
 
+// konten 4 
 
 var divFour1 = document.querySelectorAll('div')[19]
 divFour1.className = "container-fluid bgr2"
@@ -99,10 +100,10 @@ divFour4.className = "col-md-4 kotak"
 var divFour5 = document.querySelectorAll('div')[23]
 divFour5.className = "col-md-6 kotak2"
 
-var divFour6 =document.querySelectorAll('div')[24]
-divFour6.className  = "row"
+var divFour6 = document.querySelectorAll('div')[24]
+divFour6.className = "row"
 
-var divFour7 =document.querySelectorAll('div')[25]
+var divFour7 = document.querySelectorAll('div')[25]
 divFour7.className = "col-md-6 kotak2"
 
 var divFour8 = document.querySelectorAll('div')[26]
@@ -113,4 +114,3 @@ divFour9.className = "row"
 
 var divFour10 = document.querySelectorAll('div')[28]
 divFour10.className = "col-md-4 kotak"
-
