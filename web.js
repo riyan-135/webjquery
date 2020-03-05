e76b6729a928b11e55c533707a07e4c720746ffb
ryan <riyan-135>
2020-03-05 17:52:26 +0700

    done

--- web.js
+++ web.js
@@ -1,50 +1,50 @@
-// konten 1
-var first_a = document.querySelectorAll('div')[0]
-first_a.className = "container-fluid gambar"
+// // konten 1
+// var first_a = document.querySelectorAll('div')[0]
+// first_a.className = "container-fluid gambar"
 
-// css konten 1
-var first_aCSS = document.querySelector('.gambar')
-first_aCSS.style.backgroundImage = "url(program1.jpg)"
-first_aCSS.style.minHeight = "500px"
-first_aCSS.style.backgroundAttachment = "fixed"
-first_aCSS.style.backgorundRepeat ="no-repeat"
-first_aCSS.style.backgroundSize = "cover"
-first_aCSS.style.color = "white"
-first_aCSS.style.opacity = "0.8"
+// // css konten 1
+// var first_aCSS = document.querySelector('.gambar')
+// first_aCSS.style.backgroundImage = "url(program1.jpg)"
+// first_aCSS.style.minHeight = "500px"
+// first_aCSS.style.backgroundAttachment = "fixed"
+// first_aCSS.style.backgorundRepeat ="no-repeat"
+// first_aCSS.style.backgroundSize = "cover"
+// first_aCSS.style.color = "white"
+// first_aCSS.style.opacity = "0.8"
 
-var second_a = document.querySelectorAll('div')[1]
-second_a.className ="row"
+// var second_a = document.querySelectorAll('div')[1]
+// second_a.className ="row"
 
-var third_a = document.querySelectorAll('div')[2]
-third_a.className = "col-md-12"
+// var third_a = document.querySelectorAll('div')[2]
+// third_a.className = "col-md-12"
 
-var four_a = document.querySelectorAll('div')[3]
-four_a.className = "text-center mt-5 mb-5"
+// var four_a = document.querySelectorAll('div')[3]
+// four_a.className = "text-center mt-5 mb-5"
 
-var first_h1 = document.querySelectorAll('h1')[0]
-first_h1.className = "display-3"
+// var first_h1 = document.querySelectorAll('h1')[0]
+// first_h1.className = "display-3"
 
-var second_h1 = document.querySelectorAll('h1')[1]
-second_h1.className = "display-3"
+// var second_h1 = document.querySelectorAll('h1')[1]
+// second_h1.className = "display-3"
 
-// css button
-    var first_button = document.querySelectorAll('button')[0]
-    first_button.className = "daftar"
-    first_button.style.backgroundColor = "blue"
-    first_button.style.border = "none"
-    first_button.style.color = "white"
-    first_button.style.padding = "20px"
-    first_button.style.textAlign = "center"
-    first_button.style.textDecoration = "none"
-    first_button.style.display = "inline-block"
-    first_button.style.fontSize = "16px"
-    first_button.style.margin = "4px 2px"
-    first_button.style.borderRadius = "40px"
+// // css button
+//     var first_button = document.querySelectorAll('button')[0]
+//     first_button.className = "daftar"
+//     first_button.style.backgroundColor = "blue"
+//     first_button.style.border = "none"
+//     first_button.style.color = "white"
+//     first_button.style.padding = "20px"
+//     first_button.style.textAlign = "center"
+//     first_button.style.textDecoration = "none"
+//     first_button.style.display = "inline-block"
+//     first_button.style.fontSize = "16px"
+//     first_button.style.margin = "4px 2px"
+//     first_button.style.borderRadius = "40px"
 
-    // css icon play
-var first_i = document.querySelectorAll('i')[0]
-first_i.className = "fa fa-play-circle"
-first_i.style.fontSize = "72px"
-first_i.style.color = "white"
+//     // css icon play
+// var first_i = document.querySelectorAll('i')[0]
+// first_i.className = "fa fa-play-circle"
+// first_i.style.fontSize = "72px"
+// first_i.style.color = "white"
 
-// konten 1 selesai //
+// // konten 1 selesai //
